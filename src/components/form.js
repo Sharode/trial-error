a01db285fea36e2613c80e93de19e1b8177cbbd8
Neil Williams <Williams.Sharode@gmail.com>
2020-04-10 19:03:05 -0400

    fixing form

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function form() {
 	return (
-		<form name="contact" method="POST" action="/page-2" data-netlify="true" netlify>
+		<form name="contact" action="/page-2" data-netlify="true" netlify>
 			<label htmlFor=""> First Name</label>
 			<input type="text" />
 			<label htmlFor=""> Last Name</label>
